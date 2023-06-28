2c8962eb49f4502cdc7e1f5374a2decb17c96606
Sushij <sushant.wurk@gmail.com>
2023-06-28 22:11:24 +0530

    Add. spec and entry is a map

--- lib/todolist/todolist.ex
+++ lib/todolist/todolist.ex
@@ -1,11 +1,16 @@
 defmodule Todolist.Todolist do
   alias Todolist.MultiDict
+  @type todo_list() :: %{date: Date.t(), title: String.t()}
+  @type entry() :: %{date: Date.t(), title: String.t()}
+
   def new(), do: MultiDict.new()
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  @spec add_entry(todo_list(), entry()) :: todo_list()
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
+  @spec entries(todo_list(), Date.t()) :: String.t()
   def entries(todo_list, date) do
     MultiDict.get(todo_list, date)
   end
